4ee2a20f2718d3607b14cc5679cc78539565b880
Javone <javosmith@indigoempirellc.com>
2025-09-29 14:35:31 -0400

    Added html structure for button container and display container. Created function to create calc display.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,22 @@
+let mainContainer = document.querySelector('.main-container')
+let display = document.querySelector('#display')
+
+
+function createCalc(){
+    for(let x = 5; x > 0; x--){
+        let btnContainer = document.createElement('div')
+        mainContainer.append(btnContainer)
+        for(let y = 4; y > 0; y--){
+            let btn = document.createElement('button')
+            btn.innerText = 'button'
+            btn.setAttribute('class','btn')
+            btnContainer.append(btn)
+        }
+    }
+}
+
+createCalc()
+
 const add = (a,b) => a + b
 const subtract = (a,b) => b - a
 const multiply = (a,b) => a * b
@@ -22,9 +41,11 @@ function operate(a,b,operator){
         case '/':
             console.log(divide(a,b))
             break;
-        default:
-            break;
-    }
-}
-
+            default:
+                break;
+            }   
+        }
+                    
 operate(n1,n2,operand)
+
+display.append(n1)
